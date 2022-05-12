5d504cd921f704d3a98482ead49ed538672cbdf5
Anh Tran <anhtnt@elightup.com>
2022-05-12 09:17:26 +0700

    Optimize code for Data\Fields

--- src/Processors/Data/Fields.php
+++ src/Processors/Data/Fields.php
@@ -5,16 +5,27 @@ class Fields {
 	private $parent;
 	private $storage;
 	private $field;
+	private $all_field_settings;
 
 	public function __construct( $parent, $storage ) {
 		$this->parent  = $parent;
 		$this->storage = $storage;
+
+		$this->all_field_settings = $this->get_all_field_settings();
+	}
+
+	private function get_all_field_settings() {
+		$fields   = get_option( 'wpcf-fields' ) ?: [];
+		$termmeta = get_option( 'wpcf-termmeta' ) ?: [];
+		$usermeta = get_option( 'wpcf-usermeta' ) ?: [];
+
+		return array_merge( $fields, $termmeta, $usermeta );
 	}
 
 	public function migrate_fields() {
 		foreach( $this->parent as $field_group_id ) {
 			$fields = get_post_meta( $field_group_id, '_wp_types_group_fields', true );
-			$fields = array_filter( explode( ",", $fields ) );
+			$fields = array_filter( explode( ',', $fields ) );
 			foreach ( $fields as $field ) {
 				$this->field = $field;
 				$this->migrate_field();
@@ -22,23 +33,18 @@ class Fields {
 		}
 	}
 
-	private function get_fields_settings() {
-		$fields   = get_option( 'wpcf-fields' ) ?: [];
-		$termmeta = get_option( 'wpcf-termmeta' ) ?: [];
-		$usermeta = get_option( 'wpcf-usermeta' ) ?: [];
-
-		$settings = array_merge( $fields, $termmeta, $usermeta );
-		return $settings;
-	}
-
 	private function migrate_field() {
-		$settings = $this->get_fields_settings();
-		$settings = $settings[ $this->field ];
+		$settings = $this->all_field_settings[ $this->field ] ?? null;
+		if ( ! $settings ) {
+			return;
+		}
 
 		$ignore_types = [ 'skype', 'post' ];
 		if ( in_array( $settings['type'], $ignore_types ) ) {
 			return;
 		}
+
+		$field_id = '';
 		if ( preg_match( '/^_repeatable_group_/', $this->field ) ) {
 			$field_id = explode( '_', $this->field );
 			$field_id = (int) end( $field_id );
@@ -47,7 +53,7 @@ class Fields {
 		$args = [
 			'settings' => $settings,
 			'storage'  => $this->storage,
-			'field_id' => !empty( $field_id ) ? $field_id : ''
+			'field_id' => $field_id,
 		];
 
 		$field_type = new FieldType( $args );
