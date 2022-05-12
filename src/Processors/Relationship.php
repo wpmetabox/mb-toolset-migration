62c2c63c4a70c2965b786fadd763db6d2916af59
Anh Tran <anhtnt@elightup.com>
2022-05-12 10:18:46 +0700

    Optimize code for relationships

--- src/Processors/Relationship.php
+++ src/Processors/Relationship.php
@@ -1,53 +1,39 @@
 <?php
 namespace MetaBox\TS\Processors;
 
-use MetaBox\Support\Arr;
-
 class Relationship extends Base {
-
 	protected function get_items() {
-
-		$items = $this->get_id_relationship();
-		return $items;
-	}
-
-	private function get_id_relationship() {
 		global $wpdb;
 		$sql = "SELECT id FROM `{$wpdb->prefix}toolset_relationships` WHERE origin='wizard'";
-		return $wpdb->get_col( $wpdb->prepare( $sql ) );
+		return $wpdb->get_col( $sql );
 	}
 
-	private function get_value_relationship( $table, $value, $where, $value2 ) {
-		global $wpdb;
-		$sql = "SELECT `{$value}`  FROM `{$wpdb->prefix}{$table}` WHERE `{$where}`=%s";
-		return $wpdb->get_var( $wpdb->prepare( $sql, $value2 ) );
-	}
-
-	private function get_value_associations( $table, $value, $where, $value2 ) {
-		global $wpdb;
-		$sql = "SELECT `{$value}`  FROM `{$wpdb->prefix}{$table}` WHERE `{$where}`=%s";
-		return $wpdb->get_col( $wpdb->prepare( $sql, $value2 ) );
+	protected function migrate_item() {
+		$post_id = $this->create_post( $this->item );
+		$this->migrate_settings( $this->item, $post_id );
+		$this->migrate_values( $this->item );
+		$this->disable_post( $this->item );
 	}
 
 	private function create_post( $id ) {
-		$title  = $this->get_value_relationship( 'toolset_relationships', 'display_name_plural', 'id', $id );
-		$status = $this->get_value_relationship( 'toolset_relationships', 'is_active', 'id', $id );
-		$slug   = $this->get_value_relationship( 'toolset_relationships', 'slug', 'id', $id );
+		$title  = $this->get_col_single_value( 'toolset_relationships', 'display_name_plural', 'id', $id );
+		$status = $this->get_col_single_value( 'toolset_relationships', 'is_active', 'id', $id );
+		$slug   = $this->get_col_single_value( 'toolset_relationships', 'slug', 'id', $id );
 		$data   = [
 			'post_title'  => $title,
 			'post_type'   => 'mb-relationship',
-			'post_status' => ( $status == '1' ) ? 'publish' : 'draft',
+			'post_status' => $status == '1' ? 'publish' : 'draft',
 			'post_name'   => $slug
 		];
 
-		$relationship_id = $this->get_value_relationship( 'posts', 'ID', 'post_name', $slug );
-		if ( $relationship_id ) {
-			$data['ID'] = $relationship_id;
+		$post_id = $this->get_col_single_value( 'posts', 'ID', 'post_name', $slug );
+		if ( $post_id ) {
+			$data['ID'] = $post_id;
 			wp_update_post( $data );
 		} else {
-			$relationship_id = wp_insert_post( $data );
+			$post_id = wp_insert_post( $data );
 		}
-		return $relationship_id;
+		return $post_id;
 	}
 
 	private function disable_post( $id ) {
@@ -56,75 +42,70 @@ class Relationship extends Base {
 		$wpdb->query( $wpdb->prepare( $sql, $id ) );
 	}
 
-	private function migrate_settings( $id, $relationship_id ) {
-		$title        = $this->get_value_relationship( 'toolset_relationships', 'display_name_plural', 'id', $id );
-		$slug         = $this->get_value_relationship( 'toolset_relationships', 'slug', 'id', $id );
-		$parent       = $this->get_value_relationship( 'toolset_relationships', 'parent_types', 'id', $id );
-		$parent_type  = $this->get_value_relationship( 'toolset_type_sets', 'type', 'set_id', $parent );
-		$child        = $this->get_value_relationship( 'toolset_relationships', 'child_types', 'id', $id );
-		$child_type   = $this->get_value_relationship( 'toolset_type_sets', 'type', 'set_id', $child );
+	private function migrate_settings( $id, $post_id ) {
+		$title        = $this->get_col_single_value( 'toolset_relationships', 'display_name_plural', 'id', $id );
+		$slug         = $this->get_col_single_value( 'toolset_relationships', 'slug', 'id', $id );
+		$from_post    = $this->get_col_single_value( 'toolset_relationships', 'parent_types', 'id', $id );
+		$from_type    = $this->get_col_single_value( 'toolset_type_sets', 'type', 'set_id', $from_post );
+		$to_post      = $this->get_col_single_value( 'toolset_relationships', 'child_types', 'id', $id );
+		$to_type      = $this->get_col_single_value( 'toolset_type_sets', 'type', 'set_id', $to_post );
 		$relationship = [
 			'id'         => $slug,
 			'menu_title' => $title,
-			'from'       => $parent_type,
-			'to'         => $child_type,
+			'from'       => $from_type,
+			'to'         => $to_type,
 		];
 		$settings    = [
 			'id'         => $slug,
 			'menu_title' => $title,
 			'from'       => [
 				'object_type' => 'post',
-				'post_type'=> $parent_type,
-				'taxonomy' => 'category',
+				'post_type'   => $from_type,
+				'taxonomy'    => 'category',
 			],
 			'to'         => [
 				'object_type' => 'post',
-				'post_type'=> $child_type,
-				'taxonomy' => 'category',
+				'post_type'   => $to_type,
+				'taxonomy'    => 'category',
 			],
 		];
-		update_post_meta( $relationship_id, 'relationship', $relationship );
-		update_post_meta( $relationship_id, 'settings', $settings );
+		update_post_meta( $post_id, 'relationship', $relationship );
+		update_post_meta( $post_id, 'settings', $settings );
 	}
 
 	private function migrate_values( $id ) {
-		$parent_id      = $this->get_value_associations( 'toolset_associations', 'parent_id', 'relationship_id', $id );
+		$parent_id      = $this->get_col_values( 'toolset_associations', 'parent_id', 'relationship_id', $id );
 		$element_parent = [];
 		foreach( $parent_id as $value ) {
-			$element_parent[] = $this->get_value_relationship( 'toolset_connected_elements', 'element_id', 'group_id', $value );
+			$element_parent[] = $this->get_col_single_value( 'toolset_connected_elements', 'element_id', 'group_id', $value );
 		}
-		$child_id      = $this->get_value_associations( 'toolset_associations', 'child_id', 'relationship_id', $id );
+		$child_id      = $this->get_col_values( 'toolset_associations', 'child_id', 'relationship_id', $id );
 		$element_child = [];
 		foreach( $child_id as $value ) {
-			$element_child[] = $this->get_value_relationship( 'toolset_connected_elements', 'element_id', 'group_id', $value );
+			$element_child[] = $this->get_col_single_value( 'toolset_connected_elements', 'element_id', 'group_id', $value );
 		}
-		$slug = $this->get_value_relationship( 'toolset_relationships', 'slug', 'id', $id );
+		$slug = $this->get_col_single_value( 'toolset_relationships', 'slug', 'id', $id );
+
 		global $wpdb;
 		$sql  = "INSERT INTO `{$wpdb->prefix}mb_relationships` ( `from`, `to`, `type` ) VALUES ( %d, %d, %s)";
-		$from = $this->get_value_associations( 'mb_relationships', 'from', 'type', $slug );
-		foreach( $element_parent as $key => $value ) {
+		$from = $this->get_col_values( 'mb_relationships', 'from', 'type', $slug );
+
+		foreach ( $element_parent as $key => $value ) {
 			if ( !in_array( $value, $from ) ) {
 				$wpdb->query( $wpdb->prepare( $sql, (int)$value, (int)$element_child[$key], $slug ) );
 			}
 		}
 	}
 
-	protected function migrate_item() {
-		$this->migrate_relationship();
+	private function get_col_single_value( $table, $col, $conditional_col, $conditional_value ) {
+		global $wpdb;
+		$sql = "SELECT `{$col}` FROM `{$wpdb->prefix}{$table}` WHERE `{$conditional_col}`=%s LIMIT 1";
+		return $wpdb->get_var( $wpdb->prepare( $sql, $conditional_value ) );
 	}
 
-	private function migrate_relationship() {
-		$items = $this->get_items();
-		foreach ( $items as $id ) {
-			$relationship_id = $this->create_post( $id );
-			$this->migrate_settings( $id, $relationship_id );
-			$this->migrate_values( $id );
-			$this->disable_post( $id );
-		}
-		wp_send_json_success( [
-			'message' => __( 'Done', 'mb-toolset-migration' ),
-			'type'    => 'done',
-		] );
+	private function get_col_values( $table, $col, $conditional_col, $conditional_value ) {
+		global $wpdb;
+		$sql = "SELECT `{$col}`  FROM `{$wpdb->prefix}{$table}` WHERE `{$conditional_col}`=%s";
+		return $wpdb->get_col( $wpdb->prepare( $sql, $conditional_value ) );
 	}
-
 }
